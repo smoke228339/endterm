ff23812ebf6d70756572e1302f508d37de7ef68c
mansur <mansurorinbasar70@gmail.com>
2025-02-10 08:18:02 +0500

    created product Category

---
+++ src/entities/Category.java
@@ -0,0 +1,31 @@
+package entities;
+
+public class Category {
+    private int id;
+    private String name;
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Category(String name) {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Category{id=%d, name='%s'}", id, name);
+    }
+}
